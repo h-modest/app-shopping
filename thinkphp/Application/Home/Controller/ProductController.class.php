c66a1a5eb851fd365d065201713442b1aae11bb5
h-modest <h-modest@hotmail.com>
2017-07-18 11:21:46 +0800

    controller notes

--- thinkphp/Application/Home/Controller/ProductController.class.php
+++ thinkphp/Application/Home/Controller/ProductController.class.php
@@ -7,5 +7,14 @@ class ProductController extends Controller {
       $_GET = I('get.id');
       $this->assign('index', '1');
       $this->display();
+      // header("Access-Control-Allow-Origin: *");
+      // header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+      // header('Access-Control-Allow-Methods: GET, POST, PUT');
+      // $result = array(
+      //   'flag' => 'Success',
+      //   'msg' => '错误',
+      //   'data' => 'asdf'
+      // );
+      // $this->ajaxReturn(json_encode($result),'JSON'); //接口数据推送
     }
 }
