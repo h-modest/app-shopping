06d5b3a662bc91a56f7133af181ad36eae8ba234
h-modest <h-modest@hotmail.com>
2017-08-02 08:42:12 +0800

    update

--- thinkphp/Application/Home/Controller/ProductController.class.php
+++ thinkphp/Application/Home/Controller/ProductController.class.php
@@ -4,17 +4,16 @@ use Common\Controller\BaseController as Controller;
 use Common;
 class ProductController extends Controller {
     public function index(){
-      $_GET = I('get.id');
-      $this->assign('index', '1');
-      $this->display();
+      // $_GET = I('get.id');
+      // $this->assign('index', '1');
+      // $this->display();
       // header("Access-Control-Allow-Origin: *");
       // header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
       // header('Access-Control-Allow-Methods: GET, POST, PUT');
-      // $result = array(
-      //   'flag' => 'Success',
-      //   'msg' => '错误',
-      //   'data' => 'asdf'
-      // );
-      // $this->ajaxReturn(json_encode($result),'JSON'); //接口数据推送
+      $callback = $_GET['callback'];
+      //json数据
+      $json_data = '["customername1","customername2"]';
+      //输出jsonp格式的数据
+      echo $callback . "(" . $json_data . ")";
     }
 }
